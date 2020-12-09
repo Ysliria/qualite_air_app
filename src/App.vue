a34eb3cfe2f1fe06c7246e19ae8fd459bbdc072e
mauger <mickael.auger@apside-groupe.com>
2020-12-09 08:36:22 +0100

    [Cours_23] Mise en place du projet

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Mesure de la qualité de l'air</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+</style>
