152599e25ead5a2f460ef8c1fba4600f66afe2fe
mauger <mickael.auger@apside-groupe.com>
2020-12-10 08:34:10 +0100

    [Cours_26] Composant, données tableau, condition

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,33 @@
   <div id="app">
     <div class="container">
       <h1>Mesure de la qualité de l'air</h1>
+      <div class="row">
+        <div v-for="city of cities" :key="city.index"  class="col-sm-4">
+          <City :city="city"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App'
+import City from '@/components/City';
+
+export default {
+  name:     'App',
+  components: {
+    City,
+  },
+  data() {
+    return {
+      cities: [
+        {name: 'Lyon', iqa: 40},
+        {name: 'Paris', iqa: 70},
+        {name: 'Brest', iqa: 10}
+      ]
+    };
   }
+};
 </script>
 
 <style>
---
+++ src/components/City.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <b-card :title="city.name" :class="color">
+      <b-card-text>
+        Qualité de l'air {{ city.iqa }}
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    city: {
+      type:    Object,
+      default: null
+    },
+  },
+  data() {
+    return {
+      color: ''
+    };
+  },
+  mounted() {
+    if (this.city.iqa <= 30) {
+      this.color = 'pollution-faible';
+    }
+
+    if (this.city.iqa > 30 && this.city.iqa <= 50) {
+      this.color = 'pollution-medium';
+    }
+
+    if (this.city.iqa > 50) {
+      this.color = 'pollution-forte';
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .pollution-faible {
+    background: green;
+    color: white;
+  }
+
+  .pollution-medium {
+    background: orange;
+    color: white;
+  }
+
+  .pollution-forte {
+    background: red;
+    color: white;
+  }
+</style>
